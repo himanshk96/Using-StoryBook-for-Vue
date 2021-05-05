dc9fb44d76245f47c01ce2da1abf998a31c1adb2
himanshu kriplani <kriplani@usc.edu>
2021-05-05 01:24:27 -0700

    CSS change checkpoint

--- src/components/checkbox.jsx
+++ src/components/checkbox.jsx
@@ -11,26 +11,66 @@ const Label = styled('label')`
 `
 const Checkbox = styled('input')
 `
-    &:hover {
-        outline:none;
-        box-shadow: 0 0 2px 2px rgb(200,200,200);
-        border-radius: 4px;
-    }
+    // margin: 0;
+    // appearance: none;
+    // display: block;
+    width: 18px;
+    height: 18px;
+    background-color: #fff;
+    border: 2px solid #aaa;
+    border-radius: 4px;
+    // outline: none;
 
-    &:disabled{
-        border:3px solid rgb(45,92,197);
-    }
+    &:checked {
+      position: relative;
+      background-color: #5290ff;
+      border-color: transparent;
 
-    &:disabled {
-        border-radius: 4px;
-        border:3px solid rgb(45,92,197);
+      /* The checkmark */
+      &::after {
+        // box-sizing: border-box;
+        content: '';
+        position: absolute;
+        width: 6px;
+        height: 10px;
+        // border: 2px solid #fff;
+        border-top: none;
+        border-left: none;
+        // transform-origin: bottom right;
+        // transform:
+        //   translateX(0)
+        //   translateY(1px)
+        //   rotate(45deg);
+        
+      }
     }
 
     &:focus {
-        border:1px solid rgb(45,92,197);
-        padding: 10px;
+      box-shadow: 0 0 0 3px #8cdaff;
     }
-    transform: scale(2);
+    &:hover {
+        box-shadow: 0 0 0 3px #8cdaff;
+      }
+    // &:hover {
+    //     outline:none;
+    //     box-shadow: 0 0 2px 2px rgb(200,200,200);
+    //     border-radius: 4px;
+    // }
+
+    // &:disabled{
+    //     border:3px solid rgb(45,92,197);
+    // }
+
+    // &:disabled {
+    //     border-radius: 4px;
+    //     border:3px solid rgb(45,92,197);
+    // }
+
+    // &:focus {
+    //     border:1px solid rgb(45,92,197);
+    //     padding: 10px;
+    // }
+    // transform: scale(2);
 `
 
 export default {
