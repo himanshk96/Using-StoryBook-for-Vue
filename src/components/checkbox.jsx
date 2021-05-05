6845504943a193e60e917c09be7dcf67165bee59
himanshu kriplani <kriplani@usc.edu>
2021-05-05 02:12:56 -0700

    CSS cleanup and color update

--- src/components/checkbox.jsx
+++ src/components/checkbox.jsx
@@ -10,116 +10,82 @@ const Label = styled('label')`
     margin-left: 8px;
 `
 const Checkbox = styled('input')
-`
-    // margin: 0;
-    // appearance: none;
-    // display: block;
-    width: 18px;
-    height: 18px;
-    background-color: #fff;
-    border: 2px solid #aaa;
+    `
+    width: 20px;
+    height: 20px;
+    background-color: silver;
+    border: 2px solid silver;
     border-radius: 4px;
-    // outline: none;
-
+    
     &:checked {
-      position: relative;
-      background-color: #5290ff;
-      border-color: transparent;
-
-      /* The checkmark */
-      &::after {
-        // box-sizing: border-box;
-        content: '';
-        position: absolute;
-        width: 6px;
-        height: 10px;
-        // border: 2px solid #fff;
-        border-top: none;
-        border-left: none;
-        // transform-origin: bottom right;
-        // transform:
-        //   translateX(0)
-        //   translateY(1px)
-        //   rotate(45deg);
-        
-      }
+        bg-color: white;
+        &::after {
+            border: 2px solid white;
+        }
+        &:focus {
+            outline: 5px solid #0375FF;
+            border-color:white;
+        }
+        &:hover {
+            outline:none;
+            box-shadow: 0 0 0 3px silver;
+        }
     }
-
     &:focus {
-      box-shadow: 0 0 0 3px #8cdaff;
+      border: 5px solid silver; 
     }
     &:hover {
-        box-shadow: 0 0 0 3px #8cdaff;
-      }
-    // &:hover {
-    //     outline:none;
-    //     box-shadow: 0 0 2px 2px rgb(200,200,200);
-    //     border-radius: 4px;
-    // }
-
-    // &:disabled{
-    //     border:3px solid rgb(45,92,197);
-    // }
-
-    // &:disabled {
-    //     border-radius: 4px;
-    //     border:3px solid rgb(45,92,197);
-    // }
-
-    // &:focus {
-    //     border:1px solid rgb(45,92,197);
-    //     padding: 10px;
-    // }
-    // transform: scale(2);
+        box-shadow: 0 0 0 3px silver;
+    }
 `
 
 export default {
-   
+
     name: 'Checkbox',
-    
+
     props: {
         checkbox: {
-          type: Object,
-          required: true,
-          default: () => ({   title: '',disable:'' }),
-          validator: checkbox => ['title','disable'].every(key => key in checkbox),
+            type: Object,
+            required: true,
+            default: () => ({ title: '', disable: '' }),
+            validator: checkbox => ['title', 'disable'].every(key => key in checkbox),
         },
     },
 
-    created(){
-        
+    created() {
+
     },
     mounted() {
         this.$nextTick(() => {
             console.log(this);
-        console.log(this.$refs);
-        console.log(this.$refs.ci);
-        // this.$refs['ci'].indeterminate=true;
-        
-        })
-      }
-      ,methods:{
-        
-        handleChange(e){
-            // console.log("This change")
-            console.log(this.checkbox.hyphenated,this.$refs['ci'].checked)
-            
-            if (this.checkbox.hyphenated && this.$refs['ci'].checked)
-                this.$refs['ci'].indeterminate=true;
-        }
-      },
+            console.log(this.$refs);
+            console.log(this.$refs.ci);
+            // this.$refs['ci'].indeterminate=true;
 
-    render(){
+        })
+    }
+    , methods: {
+
+        handleChange(e) {
+            // console.log("This change")
+            console.log(this.checkbox.hyphenated, this.$refs['ci'].checked)
+
+            if (this.checkbox.hyphenated && this.$refs['ci'].checked)
+                this.$refs['ci'].indeterminate = true;
+        }
+    },
+
+    render() {
 
         return (
-                <div>
-                    <Checkbox ref="ci" type="checkbox" disabled={this.checkbox.disable} onChange={this.handleChange}>
-                        </Checkbox>
-                    <Label for="convrrt"> {this.checkbox.title} </Label>
-                </div>
+            <div>
+                <Checkbox ref="ci" type="checkbox" disabled={this.checkbox.disable} onChange={this.handleChange}>
+                </Checkbox>
+                <Label for="convrrt"> {this.checkbox.title} </Label>
+            </div>
         )
     },
-  };
+};
 
 
 
